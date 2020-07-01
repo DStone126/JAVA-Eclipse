68332069d7a763c493226c9a8108f406ca5b1ac5
DStone126 <dmstone0219@gmail.com>
2020-06-30 20:22:48 -0400

    Added school work

--- com/syntax/class15/StringBufferDemo.java
+++ com/syntax/class15/StringBufferDemo.java
@@ -9,6 +9,7 @@ public static void main(String [] args) {
 	
 String str2 = "Hello";//String is immutable object = not complete
 str2.concat(" friends ");//new object gets created that will have value "hello friends"--->no one takes it
+
 System.out.println(str2);//hello
 
 
---
+++ com/syntax/class16/Calculator.java
@@ -0,0 +1,35 @@
+package com.syntax.class16;
+
+public class Calculator {
+
+	//add, sub, div, multi
+	
+	void add(int a, int b) {
+		//int a = 100;
+		//int b = 200;
+		
+		System.out.println(a + b);
+	}
+	
+	void sub(int a, int b) {
+		//int a = 100;
+		//int b = 50;
+		
+		System.out.println(a - b);
+	}	
+	
+	void div(int a, int b) {
+		
+		
+		System.out.println(a / b);
+	}
+	
+	void multi(int a, int b) {
+		
+		
+		System.out.println(a * b);
+	}
+	
+	
+	
+}
